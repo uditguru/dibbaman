260cfa19c74049edc3f0cb89b4a81697a88a81bb
Udit <guru.udit@gmail.com>
2016-04-21 11:23:42 +0530

    12
    2222

--- assets/js/map.js
+++ assets/js/map.js
@@ -3,15 +3,15 @@ jQuery(document).ready(function(){
 
     map = new GMaps({
         div: '#map',
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 22.6861341,
+        lng: 75.8677397,
     });
     map.addMarker({
-        lat: 51.451573,
-        lng: -2.595008,
+        lat: 22.6861341,
+        lng: 75.8677397,
         title: 'Address',      
         infoWindow: {
-            content: '<h5 class="title">College Green</h5><p><span class="region">Address line goes here</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
+            content: '<h5 class="title">Dibbaman</h5><p><span class="region">Adarsh Gram</span><br><span class="postal-code">Postcode</span><br><span class="country-name">Country</span></p>'
         }
         
     });
